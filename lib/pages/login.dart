48d0eec2a166f03024229531aeb8daa651a28d39
Sajad Nowroozi <sajad.nowroozi5442@gmail.com>
2024-03-26 01:45:57 +0430

    now finished all page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,147 +12,154 @@ class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 60.0, left: 30.0),
-              height: MediaQuery.of(context).size.height / 2,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFB91635),
-                    Color(0xFF621d3c),
-                    Color(0xFF311937),
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          child: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 60.0, left: 30.0),
+                height: MediaQuery.of(context).size.height / 1,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFB91635),
+                      Color(0xFF621d3c),
+                      Color(0xFF311937),
+                    ],
+                  ),
+                ),
+                child: Text(
+                  "Hello\nSign in!",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              child: Text(
-                "Hello\nSign in!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                  top: 40.0, left: 30.0, right: 30.0, bottom: 30.0),
-              margin:
-                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 4),
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40.0),
-                  topRight: Radius.circular(40.0),
+              Container(
+                padding: EdgeInsets.only(
+                    top: 40.0, left: 30.0, right: 30.0, bottom: 30.0),
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 5),
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.0),
+                    topRight: Radius.circular(40.0),
+                  ),
                 ),
-              ),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Email",
-                      style: TextStyle(
-                          color: Color(0xFFB91635),
-                          fontSize: 23.0,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: "Email",
-                          prefixIcon: Icon(Icons.email_outlined)),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Password",
-                          prefixIcon: Icon(Icons.password_outlined)),
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Forgot password?",
-                          style: TextStyle(
-                              color: Color(0xFF311937),
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 60.0,
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 10.0),
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color(0xFFB91635),
-                            Color(0xFF621d3c),
-                            Color(0xFF311937),
-                          ],
-                        ),
-                        borderRadius: BorderRadius.circular(30.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Email",
+                        style: TextStyle(
+                            color: Color(0xFFB91635),
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.w500),
                       ),
-                      child: Center(
-                        child: Text(
-                          "SIGN IN",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold),
-                        ),
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: "Email",
+                            prefixIcon: Icon(Icons.email_outlined)),
                       ),
-                    ),
-                    Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "don't have account?",
-                          style: TextStyle(
-                              color: Color(0xFF311937),
-                              fontSize: 17.0,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => SignUp(),
-                          ),
-                        );
-                      },
-                      child: Row(
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Password",
+                            prefixIcon: Icon(Icons.password_outlined)),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           Text(
-                            "Sign Up",
+                            "Forgot password?",
                             style: TextStyle(
-                                color: Color(0xFF621d3c),
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.bold),
+                                color: Color(0xFF311937),
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500),
                           ),
                         ],
                       ),
-                    ),
-                  ]),
-            ),
-          ],
+                      SizedBox(
+                        height: 60.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFB91635),
+                              Color(0xFF621d3c),
+                              Color(0xFF311937),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "SIGN IN",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "don't have account?",
+                            style: TextStyle(
+                                color: Color(0xFF311937),
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignUp(),
+                            ),
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  color: Color(0xFF621d3c),
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -12,147 +12,154 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 60.0, left: 30.0),
-              height: MediaQuery.of(context).size.height / 2,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFB91635),
-                    Color(0xFF621d3c),
-                    Color(0xFF311937),
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          child: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 60.0, left: 30.0),
+                height: MediaQuery.of(context).size.height / 1,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFB91635),
+                      Color(0xFF621d3c),
+                      Color(0xFF311937),
+                    ],
+                  ),
+                ),
+                child: Text(
+                  "Create Your\nAccount",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              child: Text(
-                "Create Your\nAccount",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                  top: 40.0, left: 30.0, right: 30.0, bottom: 30.0),
-              margin:
-                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 4),
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40.0),
-                  topRight: Radius.circular(40.0),
+              Container(
+                padding: EdgeInsets.only(
+                    top: 40.0, left: 30.0, right: 30.0, bottom: 30.0),
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 5),
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.0),
+                    topRight: Radius.circular(40.0),
+                  ),
                 ),
-              ),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Name",
-                      style: TextStyle(
-                          color: Color(0xFFB91635),
-                          fontSize: 23.0,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: "Name",
-                          prefixIcon: Icon(Icons.person_outline)),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    Text(
-                      "Email",
-                      style: TextStyle(
-                          color: Color(0xFFB91635),
-                          fontSize: 23.0,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: "Email",
-                          prefixIcon: Icon(Icons.email_outlined)),
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Password",
-                          prefixIcon: Icon(Icons.password_outlined)),
-                    ),
-                    SizedBox(
-                      height: 30.0,
-                    ),
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 10.0),
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color(0xFFB91635),
-                            Color(0xFF621d3c),
-                            Color(0xFF311937),
-                          ],
-                        ),
-                        borderRadius: BorderRadius.circular(30.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Name",
+                        style: TextStyle(
+                            color: Color(0xFFB91635),
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.w500),
                       ),
-                      child: Center(
-                        child: Text(
-                          "SIGN UP",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold),
-                        ),
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: "Name",
+                            prefixIcon: Icon(Icons.person_outline)),
                       ),
-                    ),
-                    Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Already have an account?",
-                          style: TextStyle(
-                              color: Color(0xFF311937),
-                              fontSize: 17.0,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LogIn(),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      Text(
+                        "Email",
+                        style: TextStyle(
+                            color: Color(0xFFB91635),
+                            fontSize: 23.0,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: "Email",
+                            prefixIcon: Icon(Icons.email_outlined)),
+                      ),
+                      SizedBox(
+                        height: 40.0,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Password",
+                            prefixIcon: Icon(Icons.password_outlined)),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFB91635),
+                              Color(0xFF621d3c),
+                              Color(0xFF311937),
+                            ],
                           ),
-                        );
-                      },
-                      child: Row(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "SIGN UP",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                      ),
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           Text(
-                            "Sign In",
+                            "Already have an account?",
                             style: TextStyle(
-                                color: Color(0xFF621d3c),
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.bold),
+                                color: Color(0xFF311937),
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.w500),
                           ),
                         ],
                       ),
-                    ),
-                  ]),
-            ),
-          ],
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LogIn(),
+                            ),
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Color(0xFF621d3c),
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
